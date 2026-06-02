50c7631eec352375653342b6804d38e850caef74
Abdulhafiz Ahmad <ahmedabdulhafiz35@gmail.com>
2026-06-02 20:35:11 +0100

    ndndn

--- Level_1_Basic/Task_3_WordCounter/word_counter.py
+++ Level_1_Basic/Task_3_WordCounter/word_counter.py
@@ -15,7 +15,7 @@ def open_file(file):
 
 def word_counter(lst):
     word_counts = Counter(lst)
-    return word_counts.most_common(10)
+    return word_counts
 
 
 print(word_counter(open_file(f)))
